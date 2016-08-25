085a0e4a4696acd62d9c140be87e42936635c4f3
Vajih Montaghami <vmontagh@uwaterloo.ca>
2016-08-25 10:49:38 -0400

    Extractor is added to convert bordeaux outut to a pair of Alloy syntax.

---
+++ src/edu/uw/ece/bordeaux/tests/BoreauxVsAlloyTest.java
@@ -0,0 +1,221 @@
+package edu.uw.ece.bordeaux.tests;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.mit.csail.sdg.alloy4.A4Reporter;
+import edu.mit.csail.sdg.alloy4.Err;
+import edu.mit.csail.sdg.alloy4.Pair;
+import edu.mit.csail.sdg.alloy4.Util;
+import edu.mit.csail.sdg.alloy4compiler.ast.Command;
+import edu.mit.csail.sdg.alloy4compiler.ast.ExprUnary;
+import edu.mit.csail.sdg.alloy4compiler.parser.CompModule;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Options;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
+import edu.mit.csail.sdg.alloy4compiler.translator.TranslateAlloyToKodkod;
+import edu.uw.ece.bordeaux.A4CommandExecuter;
+import edu.uw.ece.bordeaux.HolaReporter;
+import edu.uw.ece.bordeaux.engine.BordeauxEngine;
+import edu.uw.ece.bordeaux.util.ExtractorUtils;
+import edu.uw.ece.bordeaux.util.Utils;
+
+public class BoreauxVsAlloyTest {
+
+	final File tmpFolder = new File("tmp/test");
+
+	@Before
+	public void setUp() {
+		tmpFolder.mkdirs();
+	}
+
+	public BordeauxEngine createBordeauxEngine(A4Reporter reporter, File filepath, String commandName) {
+
+		assertNotNull(commandName);
+
+		Command command = ExtractorUtils.getCommandFromNamePainfully(filepath.getAbsolutePath(), commandName);
+		assertNotNull("Cannot find command from command name", command);
+
+		try {
+			A4CommandExecuter.get().runAlloy(filepath.getAbsolutePath(), reporter, command.label);
+			A4Solution initialSoln = reporter.getA4Solution();
+			return new BordeauxEngine(filepath, command, initialSoln);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	protected A4Solution findBoreauxNearMiss(File filepath, String commandName) {
+		final HolaReporter reporter = new HolaReporter();
+		final BordeauxEngine engine = createBordeauxEngine(reporter, filepath, commandName);
+		return engine.nextNearMiss(reporter);
+	}
+
+	protected int findAlloyMissExample(File filePath, String commandName, int maxRetry) {
+
+		int tries = 1;
+
+		final A4Solution nearMissExample = findBoreauxNearMiss(filePath, commandName);
+
+		CompModule module = null;
+		try {
+			module = (CompModule) A4CommandExecuter.get().parse(filePath.getAbsolutePath(), A4Reporter.NOP);
+		} catch (Err e) {
+			e.printStackTrace();
+		}
+
+		final Command command = module.getAllCommands().stream().filter(f -> f.label.equals(commandName)).findAny()
+				.get();
+		final Command commandNot = command.change(ExprUnary.Op.NOT.make(command.formula.pos, command.formula));
+
+		A4Options options = new A4Options();
+
+		options.solver = A4Options.SatSolver.SAT4J;
+		try {
+			A4Solution ans = TranslateAlloyToKodkod.execute_command(A4Reporter.NOP, module.getAllReachableSigs(),
+					commandNot, options);
+			while (ans.satisfiable() && tries < maxRetry) {
+
+				if (equiSAT(filePath, ExtractorUtils.convertA4SolutionToAlloySyntax(nearMissExample, false),
+						ExtractorUtils.convertA4SolutionToAlloySyntax(ans, false), commandName)) {
+					
+					System.out.println("NEAR MISS="+nearMissExample);
+					System.out.println(ans);
+					break;
+				}
+				
+				System.out.println("NEAR MISS="+nearMissExample);
+				System.out.println("--->"+ExtractorUtils.convertBordeauxSolutionToAlloySyntax(nearMissExample, new HashMap<>()));
+				System.out.println("number of tuples="+ExtractorUtils.getNumberOfTuplesFromA4Solution(nearMissExample));
+
+				System.out.println("ans->"+ans);
+				System.out.println("number of tuples="+ExtractorUtils.getNumberOfTuplesFromA4Solution(ans));
+				
+				ans = ans.next();
+				++tries;
+			}
+
+		} catch (Err e) {
+			e.printStackTrace();
+		}
+		return tries;
+
+	}
+
+	protected String findScope(File filePath, String commandName) {
+		CompModule module = null;
+		try {
+			module = (CompModule) A4CommandExecuter.get().parse(filePath.getAbsolutePath(), A4Reporter.NOP);
+		} catch (Err e) {
+			e.printStackTrace();
+		}
+
+		final Command command = module.getAllCommands().stream().filter(f -> f.label.equals(commandName)).findFirst()
+				.get();
+		return ExtractorUtils.extractScopeFromCommand(command);
+
+	}
+
+	protected boolean equiSAT(File filePath, String sol1, String sol2, String commandName) {
+
+		String filecontent = Utils.readFile(filePath.getAbsolutePath());
+		String newCommandName = "__Check_EQU__";
+		File newFileTmp = new File(filePath.getParentFile(), filePath.getName() + ".tmp.als");
+		String newContent = filecontent + "\nassert " + newCommandName + " { (" + sol1 + ") iff (" + sol2 + ")}\ncheck "
+				+ newCommandName + findScope(filePath, commandName);
+
+		boolean result = false;
+		try {
+			Util.writeAll(newFileTmp.getAbsolutePath(), newContent);
+			;
+			result = !A4CommandExecuter.get()
+					.runAlloyThenGetAnswers(newFileTmp.getAbsolutePath(), A4Reporter.NOP, newCommandName).satisfiable();
+		} catch (Err e) {
+			e.printStackTrace();
+		}
+
+		//newFileTmp.deleteOnExit();
+		
+		return result;
+
+	}
+	
+	@Test
+	public void testSimplyLinked(){
+		String content = "sig A{w: lone A}\npred p{no ^w & iden\n}\nrun p for 4";
+		File tmpFile = new File(tmpFolder, "tmp.als");
+		try {
+			Util.writeAll(tmpFile.getAbsolutePath(), content);
+		} catch (Err e) {
+			e.printStackTrace();
+			fail(e.msg);
+		}
+		
+		
+		System.out.println(findAlloyMissExample(tmpFile, "p", 100));
+		
+	}
+	
+	protected A4Solution findBordeauxExample(String content, String command){
+		File tmpFile = new File(tmpFolder, "tmp.als");
+		try {
+			Util.writeAll(tmpFile.getAbsolutePath(), content);
+		} catch (Err e) {
+			e.printStackTrace();
+			fail(e.msg);
+		}
+		
+		return findBoreauxNearMiss(tmpFile.getAbsoluteFile(), "p");
+	}
+	
+	@Test
+	public void testEmptyMapA4SolutionToBordeaux(){
+		
+		final A4Solution nearMissExample = findBordeauxExample("sig A{w: lone A}\npred p{no ^w & iden\n}\nrun p for 4", "p");
+		
+		Map<String, String> map = new HashMap<>();
+		
+		assertEquals(new Pair<String,String>("",""),ExtractorUtils.convertBordeauxSolutionToAlloySyntax(nearMissExample,map ));
+	}
+	
+	@Test
+	public void testMapA4SolutionToBordeaux(){
+		final A4Solution nearMissExample = findBordeauxExample("sig A{w: lone A}\npred p{no ^w & iden\n}\nrun p for 4 but 4 Int", "p");
+		
+		System.out.println(nearMissExample);
+		
+		Map<String, String> map = new HashMap<>();
+		map.put("$findMarginalInstances_a", "A");
+		map.put("$findMarginalInstances_a_w", "w");
+		map.put("$findMarginalInstances_a'", "A");
+		map.put("$findMarginalInstances_a_w'", "w");
+		map.put("$findMarginalInstances_a''", "A");
+		map.put("$findMarginalInstances_a_w''", "w");
+		System.out.println(ExtractorUtils.convertBordeauxSolutionToAlloySyntax(nearMissExample,map ));
+		assertEquals(new Pair<String,String>("(no A and no w)","(some A2, A3: univ | (A2, A3 in A) and (A3->A2+ A3->A3 = w)"),ExtractorUtils.convertBordeauxSolutionToAlloySyntax(nearMissExample,map ));
+	}
+	
+	@Test
+	public void tesWithPrimtMapA4SolutionToBordeaux(){
+		final A4Solution nearMissExample = findBordeauxExample("sig A{w': lone A}\npred p{no ^w' & iden\n}\nrun p for 4 but 4 Int", "p");
+		
+		System.out.println(nearMissExample);
+		
+		Map<String, String> map = new HashMap<>();
+		map.put("$findMarginalInstances_a", "A");
+		map.put("$findMarginalInstances_a_w'", "w'");
+		map.put("$findMarginalInstances_a'", "A");
+		map.put("$findMarginalInstances_a_w''", "w'");
+		map.put("$findMarginalInstances_a''", "A");
+		map.put("$findMarginalInstances_a_w'''", "w'");
+		System.out.println(ExtractorUtils.convertBordeauxSolutionToAlloySyntax(nearMissExample,map ));
+		assertEquals(new Pair<String,String>("(no A and no w')","(some A2, A3: univ | (A2, A3 in A) and (A3->A2+ A3->A3 = w')"),ExtractorUtils.convertBordeauxSolutionToAlloySyntax(nearMissExample,map ));
+	}
+	
+}
--- src/edu/uw/ece/bordeaux/util/ExtractorUtils.java
+++ src/edu/uw/ece/bordeaux/util/ExtractorUtils.java
@@ -2,12 +2,16 @@ package edu.uw.ece.bordeaux.util;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import java.util.function.BiFunction;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import edu.mit.csail.sdg.alloy4.A4Reporter;
 import edu.mit.csail.sdg.alloy4.Err;
+import edu.mit.csail.sdg.alloy4.Pair;
 import edu.mit.csail.sdg.alloy4compiler.ast.Command;
 import edu.mit.csail.sdg.alloy4compiler.ast.CommandScope;
 import edu.mit.csail.sdg.alloy4compiler.ast.Expr;
@@ -15,8 +19,9 @@ import edu.mit.csail.sdg.alloy4compiler.ast.ExprBinary;
 import edu.mit.csail.sdg.alloy4compiler.ast.ExprCall;
 import edu.mit.csail.sdg.alloy4compiler.ast.ExprList;
 import edu.mit.csail.sdg.alloy4compiler.ast.ExprUnary;
-import edu.mit.csail.sdg.alloy4compiler.ast.Sig;
 import edu.mit.csail.sdg.alloy4compiler.ast.ExprUnary.Op;
+import edu.mit.csail.sdg.alloy4compiler.ast.ExprVar;
+import edu.mit.csail.sdg.alloy4compiler.ast.Sig;
 import edu.mit.csail.sdg.alloy4compiler.ast.Sig.Field;
 import edu.mit.csail.sdg.alloy4compiler.parser.CompModule;
 import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
@@ -34,7 +39,7 @@ import edu.uw.ece.bordeaux.A4CommandExecuter;
 public class ExtractorUtils {
 
 	public static Command getCommandFromNamePainfully(String filepath, String commandName) {
-		
+
 		CompModule world = null;
 		try {
 			world = (CompModule) A4CommandExecuter.get().parse(filepath, A4Reporter.NOP);
@@ -48,31 +53,31 @@ public class ExtractorUtils {
 				return command;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	public static String extractScopeFromCommand(final String srcFile, final String commandName) {
-		
+
 		CompModule module = null;
 		try {
 			module = (CompModule) A4CommandExecuter.get().parse(srcFile, A4Reporter.NOP);
 		} catch (Err e) {
 			e.printStackTrace();
 		}
-		
-		if(module != null) {
-			
-			for(Command c : module.getAllCommands()) {
-				if(c.label.equals(commandName)) {
+
+		if (module != null) {
+
+			for (Command c : module.getAllCommands()) {
+				if (c.label.equals(commandName)) {
 					return extractScopeFromCommand(c);
 				}
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
 	 * Given a command, its scope is returned as String
 	 * 
@@ -82,11 +87,13 @@ public class ExtractorUtils {
 	public static String extractScopeFromCommand(Command command) {
 		boolean first = true;
 		StringBuilder sb = new StringBuilder();
-		if (command.overall >= 0 && ((command.intScope != null && command.intScope.bitwidth >= 0) || command.maxseq >= 0 || (command.scope != null && command.scope.size() > 0)))
+		if (command.overall >= 0 && ((command.intScope != null && command.intScope.bitwidth >= 0) || command.maxseq >= 0
+				|| (command.scope != null && command.scope.size() > 0)))
 			sb.append(" for ").append(command.overall).append(" but");
 		else if (command.overall >= 0)
 			sb.append(" for ").append(command.overall);
-		else if ((command.intScope != null && command.intScope.bitwidth >= 0) || command.maxseq >= 0 || (command.scope != null && command.scope.size() > 0))
+		else if ((command.intScope != null && command.intScope.bitwidth >= 0) || command.maxseq >= 0
+				|| (command.scope != null && command.scope.size() > 0))
 			sb.append(" for");
 		if (command.intScope != null && command.intScope.bitwidth >= 0) {
 			sb.append(" ").append(command.intScope.bitwidth).append(" int");
@@ -96,7 +103,7 @@ public class ExtractorUtils {
 			sb.append(first ? " " : ", ").append(command.maxseq).append(" seq");
 			first = false;
 		}
-		if(command.scope != null) {
+		if (command.scope != null) {
 			for (CommandScope e : command.scope) {
 				sb.append(first ? " " : ", ").append(e);
 				first = false;
@@ -108,38 +115,142 @@ public class ExtractorUtils {
 	}
 
 	public static boolean sigToBeIgnored(Sig sig) {
-		
+
 		return (sig.builtin || isOrdering(sig));
 	}
-	
+
 	private static boolean isOrdering(Sig sig) {
 		return sig.isPrivate != null && sig.isOne != null
 				&& new File(sig.pos().filename).getName().equals("ordering.als");
 	}
 
 	private static boolean isSubSig(Sig sig) {
-		return sig.isSubsig != null && sig instanceof Sig.PrimSig && !((Sig.PrimSig)sig).parent.builtin;
+		return sig.isSubsig != null && sig instanceof Sig.PrimSig && !((Sig.PrimSig) sig).parent.builtin;
 	}
 
 	public static String getCamelCase(String in) {
-		if(in == null || in.isEmpty()) return in;
-		
+		if (in == null || in.isEmpty())
+			return in;
+
 		boolean lenG1 = in.length() > 1;
 		return Character.toLowerCase(in.charAt(0)) + (lenG1 ? in.substring(1) : "");
 	}
-	
+
 	public static String getLocalSigName(String sigName) {
 		return ExtractorUtils.getCamelCase(sigName);
 	}
-	
+
 	public static String getLocalFieldName(String fieldLabel, String sigName) {
 
 		return getCamelCase(sigName) + "_" + fieldLabel;
 	}
-	
-	public static int getNumberOfTuplesFromA4Solution(A4Solution solution){
+
+	protected static List<List<ExprVar>> seperateSkolemizedVars(List<ExprVar> vars) {
+
+		List<List<ExprVar>> result = new ArrayList<>();
+		// entailing no prime
+		result.add(new ArrayList<>());
+		// entailing one prime
+		result.add(new ArrayList<>());
+		// entailing two primes
+		result.add(new ArrayList<>());
+
+		Set<String> names = vars.stream().map(v -> v.label).collect(Collectors.toSet());
+
+		for (ExprVar var : vars) {
+			String label = var.label;
+			String label_p = label + "'";
+			String label_pp = label_p + "'";
+
+			if (names.contains(label) && names.contains(label_p) && names.contains(label_pp)) {
+				result.get(0).add(var);
+			}
+
+			if (names.contains(label) && names.contains(label_p) && !names.contains(label_pp)) {
+				result.get(1).add(var);
+			}
+
+			if (names.contains(label) && !names.contains(label_p) && !names.contains(label_pp)) {
+				result.get(2).add(var);
+			}
+		}
+
+		return Collections.unmodifiableList(result);
+
+	}
+
+	protected static boolean isSig(A4Solution solution, String name) {
+
+		return solution.getAllReachableSigs().makeCopy().stream().anyMatch(s -> s.label.equals("this/" + name));
+	}
+
+	protected static String convertBordeauxSolutionToAlloySyntax(A4Solution solution, List<ExprVar> vars,
+			Map<String, String> decodeSkolemizedNames) {
+
+		final List<String> declPart = new ArrayList<>();
+		final List<String> noPart = new ArrayList<>();
+		final List<String> bodyPart = new ArrayList<>();
+
+		for (ExprVar var : vars) {
+			try {
+				if (!decodeSkolemizedNames.containsKey(var.label)) {
+					System.err.println(var.label + " is not in the given map!");
+					continue;
+				}
+
+				if (solution.eval(var).toString().equals("{}")) {
+					// not atom or tuple
+					noPart.add("no " + decodeSkolemizedNames.get(var.label));
+
+				} else {
+
+					if (isSig(solution, decodeSkolemizedNames.get(var.label))) {
+						declPart.add("some " + solution.eval(var).toString().replaceAll("\\$|\\{|\\}", "") + ": univ ");
+						bodyPart.add("(" + solution.eval(var).toString().replaceAll("\\$|\\{|\\}", "") + " in "
+								+ decodeSkolemizedNames.get(var.label) + ")");
+					} else {
+						bodyPart.add(
+								"(" + solution.eval(var).toString().replaceAll("\\$|\\{|\\}", "").replaceAll(",", "+")
+										+ " = " + decodeSkolemizedNames.get(var.label));
+					}
+				}
+			} catch (Err e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		String noPartString = noPart.stream().collect(Collectors.joining(" and "));
+		String declBody = declPart.size() > 0 ? declPart.stream().collect(Collectors.joining("|")) + "| "
+				+ bodyPart.stream().collect(Collectors.joining(" and ")) : "";
+
+		if (declBody.length() > 0 && noPartString.length() > 0) {
+			return "(" + declBody + ") and " + "(" + noPartString + ")";
+		} else if (declBody.length() > 0) {
+			return "(" + declBody + ")";
+		} else if (noPartString.length() > 0) {
+			return "(" + noPartString + ")";
+		} else {
+			return "";
+		}
+
+	}
+
+	public static Pair<String, String> convertBordeauxSolutionToAlloySyntax(A4Solution solution,
+			Map<String, String> decodeSkolemizedNames) {
+
+		final List<ExprVar> vars = new ArrayList<>();
+		solution.getAllSkolems().forEach(s -> vars.add(s));
+
+		List<List<ExprVar>> skolemizedVars = seperateSkolemizedVars(vars);
+
+		return new Pair<>(convertBordeauxSolutionToAlloySyntax(solution, skolemizedVars.get(0), decodeSkolemizedNames),
+				convertBordeauxSolutionToAlloySyntax(solution, skolemizedVars.get(1), decodeSkolemizedNames));
+	}
+
+	public static int getNumberOfTuplesFromA4Solution(A4Solution solution) {
 		int num = 0;
-		
+
 		for (Sig sig : solution.getAllReachableSigs()) {
 			if (sig.builtin)
 				continue;
@@ -147,11 +258,11 @@ public class ExtractorUtils {
 			// The ordering sig should be skipped
 			if (isOrdering(sig))
 				continue;
-			
-			if(!sig.label.startsWith("this/")) {
+
+			if (!sig.label.startsWith("this/")) {
 				continue;
 			}
-			
+
 			num += solution.eval(sig).size();
 			for (Field field : sig.getFields()) {
 				num += solution.eval(field).size();
@@ -159,7 +270,7 @@ public class ExtractorUtils {
 		}
 		return num;
 	}
-		
+
 	/**
 	 * Given an A4solution object from AlloyExecuter, it converts it to a Alloy
 	 * syntax
@@ -168,7 +279,8 @@ public class ExtractorUtils {
 	 * @return
 	 */
 	public static String convertA4SolutionToAlloySyntax(A4Solution solution, boolean useLocalNames) {
-		if(solution == null) return "";
+		if (solution == null)
+			return "";
 		List<String> emptySigs = new ArrayList<>();
 		List<String> constraints = new ArrayList<>();
 		List<String> quantifiers = new ArrayList<>();
@@ -179,13 +291,13 @@ public class ExtractorUtils {
 			// The ordering sig should be skipped
 			if (isOrdering(sig))
 				continue;
-			
-			// TODO: Trying to exclude atoms that are defined as signatures. Find a better condition.
-			if(!sig.label.startsWith("this/")) {
-				System.out.println("" + (sig.isAtom == null ? "not" : "") + "atom");
+
+			// TODO: Trying to exclude atoms that are defined as signatures.
+			// Find a better condition.
+			if (!sig.label.startsWith("this/")) {
 				continue;
 			}
-			
+
 			String sigName = sig.label.replace("this/", "");
 			sigName = useLocalNames ? getCamelCase(sigName) : sigName;
 
@@ -198,7 +310,7 @@ public class ExtractorUtils {
 				}
 				if (!isSubSig(sig))
 					quantifiers.add("some disj " + atoms.stream().collect(Collectors.joining(", ")) + ": univ");
-				constraints.add("\t((" + atoms.stream().collect(Collectors.joining("+")) + ") = " + sigName+")");
+				constraints.add("\t((" + atoms.stream().collect(Collectors.joining("+")) + ") = " + sigName + ")");
 			}
 		}
 
@@ -213,7 +325,8 @@ public class ExtractorUtils {
 				for (Field field : sig.getFields()) {
 					A4TupleSet fieldsTuples = solution.eval(field);
 					String fieldName = field.label;
-					fieldName = useLocalNames ? getLocalFieldName(fieldName, sig.label.replace("this/", "")) : fieldName;
+					fieldName = useLocalNames ? getLocalFieldName(fieldName, sig.label.replace("this/", ""))
+							: fieldName;
 					if (isOrdering(sig)) {
 						fieldName = (sig.label.contains("/") ? sig.label.split("/")[0] + "/" : "")
 								+ field.label.toLowerCase();
@@ -224,7 +337,8 @@ public class ExtractorUtils {
 					} else {
 						final List<String> tuples = new ArrayList<>();
 						fieldsTuples.forEach(t -> tuples.add(t.toString().replace("$", "_")));
-						constraint = "\t((" + tuples.stream().collect(Collectors.joining("+")) + ") = " + fieldName+")";
+						constraint = "\t((" + tuples.stream().collect(Collectors.joining("+")) + ") = " + fieldName
+								+ ")";
 					}
 					if (isOrdering(sig)) {
 						String orderingAtom = solution.eval(sig).iterator().next().toString().replace("$", "_");
@@ -245,66 +359,66 @@ public class ExtractorUtils {
 	}
 
 	public static String convertFormulaExprToAlloySyntax(Expr formula, boolean useLocalNames) {
-		
-		if(formula instanceof ExprList) {
+
+		if (formula instanceof ExprList) {
 
 			StringBuilder builder = new StringBuilder();
-			ExprList exprList = (ExprList)formula;
+			ExprList exprList = (ExprList) formula;
 			String operator = exprList.op.name().toLowerCase() + " ";
-			for(Expr arg : exprList.args) {
+			for (Expr arg : exprList.args) {
 				String expr = convertFormulaExprToAlloySyntax(arg, useLocalNames);
 				builder.append(operator + expr + "\n\t");
 			}
-			
+
 			builder.delete(0, operator.length());
 			return builder.toString();
 		}
 
-		if(formula instanceof ExprUnary) {
-			ExprUnary expr = (ExprUnary)formula;
+		if (formula instanceof ExprUnary) {
+			ExprUnary expr = (ExprUnary) formula;
 			String operator = "";
-			if(expr.op != Op.NOOP) {
+			if (expr.op != Op.NOOP) {
 				String label = expr.op == Op.NOT ? expr.op.name() : expr.op.toString();
 				operator = label.split("\\s")[0].toLowerCase() + " ";
 			}
-			
+
 			return operator + convertFormulaExprToAlloySyntax(expr.sub, useLocalNames);
 		}
-		
-		if(formula instanceof ExprBinary) {
-			ExprBinary expr = (ExprBinary)formula;
+
+		if (formula instanceof ExprBinary) {
+			ExprBinary expr = (ExprBinary) formula;
 			String left = convertFormulaExprToAlloySyntax(expr.left, useLocalNames);
 			String right = convertFormulaExprToAlloySyntax(expr.right, useLocalNames);
 			return left + " " + expr.op.toString() + " " + right;
 		}
 
-		if(formula instanceof ExprCall) {
-			ExprCall expr = (ExprCall)formula;
+		if (formula instanceof ExprCall) {
+			ExprCall expr = (ExprCall) formula;
 			String call = expr.fun.label.replace("this/", "");
-			
+
 			String p = "";
-			if(expr.args.size() > 0) {
+			if (expr.args.size() > 0) {
 				StringBuilder params = new StringBuilder();
-				for(Expr arg : expr.args) {
+				for (Expr arg : expr.args) {
 					params.append(", " + convertFormulaExprToAlloySyntax(arg, useLocalNames));
 				}
-				
+
 				params.delete(0, 2);
 				p = params.toString();
 			}
-		
+
 			return String.format("%s[%s]", call, p);
 		}
-		
-		if(formula instanceof Field) {
-			Field field = (Field)formula;
-			if(useLocalNames) {
+
+		if (formula instanceof Field) {
+			Field field = (Field) formula;
+			if (useLocalNames) {
 				return ExtractorUtils.getLocalFieldName(field.label, field.sig.label.replace("this/", ""));
 			}
-			
+
 			return field.label;
 		}
-		
+
 		String name = formula.toString().replace("this/", "");
 		return useLocalNames ? getCamelCase(name) : name;
 	}
